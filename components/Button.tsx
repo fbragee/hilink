a7230148cd2b7f6a16490b1b39a7a357eb748392
fbragee <145098995+fbragee@users.noreply.github.com>
2024-04-02 15:25:13 +0100

    footer section

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,11 +5,12 @@ type ButtonProps = {
 	title: string;
 	icon?: string;
 	variant: string;
+	full?: boolean;
 };
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
 	return (
 		<button
-			className={`flexCenter gap-3 rounded-full border ${variant}`}
+			className={`flexCenter gap-3 rounded-full border ${variant} $ {full && 'w-full'}`}
 			type={type}>
 			{icon && (
 				<Image
@@ -19,7 +20,9 @@ const Button = ({ type, title, icon, variant }: ButtonProps) => {
 					height={24}
 				/>
 			)}
-			<label className="bold-16 whitespace-nowrap">{title}</label>
+			<label className="bold-16 whitespace-nowrap cursor-pointer">
+				{title}
+			</label>
 		</button>
 	);
 };
