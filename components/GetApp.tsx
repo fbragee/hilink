9270bcd31d88f644ed604b4f1407be177014f9aa
fbragee <145098995+fbragee@users.noreply.github.com>
2024-04-02 14:47:02 +0100

    Features section completed

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const GetApp = () => {
-	return <div>GetApp</div>;
+	return (
+		<section className="flexCenter w-full flex-col pb-[100px]">
+			<div className="get-app">
+				<div></div>
+			</div>
+		</section>
+	);
 };
 
 export default GetApp;
